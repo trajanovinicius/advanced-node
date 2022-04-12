07d76b99dd89ed500d42261df6da73fb8f3964d6
trajanovinicius <viniluk9@gmail.com>
2022-04-12 15:34:09 -0300

    move LoadFacebookUserApi to its own file

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,39 +1,39 @@
 //Utilizando comand patterns para deixar o código mais bem escrito.
+import { AuthenticationError } from "../../../src/domain/errors";
 import { FacebookAuthentication } from "@/domain/features/facebook-authentication";
+import { LoadFacebookUserApi } from "@/data/contracts/apis";
 
 class FacebookAuthenticationService {
   constructor(private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
 
-  async perform(params: FacebookAuthentication.Params): Promise<void> {
+  async perform(
+    params: FacebookAuthentication.Params
+  ): Promise<AuthenticationError> {
     await this.loadFacebookUserApi.loadUser(params);
+    return new AuthenticationError();
   }
-}
-
-interface LoadFacebookUserApi {
-  loadUser: (params: LoadFacebookUserApi.Params) => Promise<void>;
-}
-
-namespace LoadFacebookUserApi {
-  export type Params = {
-    token: string;
-  };
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string;
+  result = undefined;
 
-  async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser(
+    params: LoadFacebookUserApi.Params
+  ): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token;
+    return this.result;
   }
 }
 
 describe("FacebookAuthenticationService", () => {
-  it("should call LoadFacebookUserApi with correct params", async () => {
+  it("should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
     const loadFacebookUserApi = new LoadFacebookUserApiSpy();
+    loadFacebookUserApi.result = undefined;
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
-    await sut.perform({ token: "any_token" });
+    const authResult = await sut.perform({ token: "any_token" });
 
-    expect(loadFacebookUserApi.token).toBe("any_token");
+    expect(authResult).toEqual(new AuthenticationError());
   });
 });
