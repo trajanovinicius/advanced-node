f76f3cdbfa5e8ca8b857faa8057bb4bd796de10c
trajanovinicius <viniluk9@gmail.com>
2022-04-13 16:55:19 -0300

    esure service calls LoadFacebookUserApi once

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -5,12 +5,14 @@ import { FacebookAuthenticationService } from "../../../src/data/services/facebo
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string;
+  callsCount = 0;
   result = undefined;
 
   async loadUser(
     params: LoadFacebookUserApi.Params
   ): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token;
+    this.callsCount++;
     return this.result;
   }
 }
@@ -23,6 +25,7 @@ describe("FacebookAuthenticationService", () => {
     await sut.perform({ token: "any_token" });
 
     expect(loadFacebookUserApi.token).toBe("any_token");
+    expect(loadFacebookUserApi.callsCount).toBe(1);
   });
   it("Should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
     const loadFacebookUserApi = new LoadFacebookUserApiSpy();
