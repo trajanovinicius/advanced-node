875d9ac4af43767fe2e99147060d644d64cd7dd2
trajanovinicius <viniluk9@gmail.com>
2022-04-19 13:24:18 -0300

    change test strategy to use jest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,35 +1,26 @@
 //Utilizando comand patterns para deixar o código mais bem escrito.
 import { AuthenticationError } from "../../../src/domain/errors";
-import { LoadFacebookUserApi } from "@/data/contracts/apis";
 import { FacebookAuthenticationService } from "../../../src/data/services/facebook-authentication";
-
-class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
-  token?: string;
-  callsCount = 0;
-  result = undefined;
-
-  async loadUser(
-    params: LoadFacebookUserApi.Params
-  ): Promise<LoadFacebookUserApi.Result> {
-    this.token = params.token;
-    this.callsCount++;
-    return this.result;
-  }
-}
 
 describe("FacebookAuthenticationService", () => {
   it("should call LoadFacebookUserApi with correct params", async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy();
+    const loadFacebookUserApi = {
+      loadUser: jest.fn(),
+    };
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
     await sut.perform({ token: "any_token" });
 
-    expect(loadFacebookUserApi.token).toBe("any_token");
-    expect(loadFacebookUserApi.callsCount).toBe(1);
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
+      token: "any_token",
+    });
+    expect(loadFacebookUserApi.loadUser).toBeCalledTimes(1);
   });
   it("Should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy();
-    loadFacebookUserApi.result = undefined;
+    const loadFacebookUserApi = {
+      loadUser: jest.fn(),
+    };
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
     const authResult = await sut.perform({ token: "any_token" });
