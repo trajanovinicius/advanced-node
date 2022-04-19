b24ee6b47c6900ff90d9ade710ec3c06a1ed9a2a
trajanovinicius <viniluk9@gmail.com>
2022-04-19 13:30:36 -0300

    chenge test strategy to use jest mock extends

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,12 +1,13 @@
 //Utilizando comand patterns para deixar o código mais bem escrito.
 import { AuthenticationError } from "../../../src/domain/errors";
 import { FacebookAuthenticationService } from "../../../src/data/services/facebook-authentication";
+import { mock } from "jest-mock-extended";
+import { LoadFacebookUserApi } from "@/data/contracts/apis";
 
 describe("FacebookAuthenticationService", () => {
   it("should call LoadFacebookUserApi with correct params", async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn(),
-    };
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>(); // faz o jest mock automático
+
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
     await sut.perform({ token: "any_token" });
@@ -17,9 +18,7 @@ describe("FacebookAuthenticationService", () => {
     expect(loadFacebookUserApi.loadUser).toBeCalledTimes(1);
   });
   it("Should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn(),
-    };
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>();
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
